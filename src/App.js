82a61fb71b63406ff39444f486a769d41a1ca644
yogeshwaran-98 <151889778+yogeshwaran-98@users.noreply.github.com>
2024-09-24 10:46:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React, { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "animate.css";
 import "lightbox2/dist/css/lightbox.min.css";
-import $ from "jquery";
 import WOW from "wowjs";
-import OwlCarousel from "react-owl-carousel";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
 import "owl.carousel";
-import CountUp from "react-countup";
-
 import Topbar from "./components/Topbar";
 import Navbar from "./components/NavbarComponent";
 import Carousel from "./components/Carousel/CarouselComponent";
@@ -21,7 +16,6 @@ import About from "./components/About";
 import Feature from "./components/Feature";
 import Service from "./components/Service";
 import Faq from "./components/Faq";
-import Blog from "./components/Blog";
 import Team from "./components/Teams";
 import Testimonial from "./components/Testimonial";
 import Footer from "./components/Footer";
